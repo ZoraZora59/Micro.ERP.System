84fd897c9396fe61dba7b93f9b97d2c0bab90f50
ZoraGK <jyh199759@126.com>
2019-04-17 17:57:51 +0800

    添加资金数据接口

---
+++ MicroERP.IDAL/IFundsData.cs
@@ -0,0 +1,30 @@
+﻿using MicroERP.Model;
+using System.Collections.Generic;
+
+namespace MicroERP.IDAL
+{
+    public interface IFundsData
+    {
+        void CreateVielationInfo(EmployeeViolationInfo employeeViolationInfo);
+        void CreateSalaryInfo(FundsSalaryInfo fundsSalaryInfo);
+        void CreateGoodsFundsRecord(FundsGoodsInfo fundsGoodsInfo);
+        void CreateAssetsRecord(TotalAssetsInfo totalAssetsInfo);
+
+        void UpdateVielationInfo(EmployeeViolationInfo employeeViolationInfo);
+        void UpdateGoodsFundsRecord(FundsGoodsInfo fundsGoodsInfo);
+
+        TotalAssetsInfo GetTotalAssetsNow();
+        EmployeeViolationInfo GetEmployeeViolationInfo(int recordID);
+        EmployeeSalary GetEmployeeSalaryBySalaryID(int salaryID);
+        EmployeeSalary GetEmployeeSalaryByUserID(int userID);
+        FundsGoodsInfo GetFundsGoodsInfo(int fundsGoodsInfo);
+
+        List<TotalAssetsInfo> GetTotalAssetsInfos();
+        List<EmployeeSalary> GetEmployeeSalaries();
+        List<EmployeeViolationInfo> GetEmployeeViolationInfosByUserID(int userID);
+        List<EmployeeViolationInfo> GetEmployeeViolationInfosByDate(int year, int month);
+        List<EmployeeViolationInfo> GetEmployeeViolationInfos();
+        List<FundsSalaryInfo> GetFundsSalaryInfos();
+        List<FundsGoodsInfo> GetFundsGoodsInfos();
+    }
+}
