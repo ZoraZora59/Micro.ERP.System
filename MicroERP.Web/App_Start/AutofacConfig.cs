ec88e37b5915c5fe2a73321f45a017cee9fe19ab
ZoraGK <jyh199759@126.com>
2019-05-06 17:10:00 +0800

    添加AutoFac

---
+++ MicroERP.Web/App_Start/AutofacConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration;
+using Autofac.Integration.Mvc;
+
+namespace MicroERP.Web.App_Start
+{
+    public class AutofacConfig
+    {
+        public static void Register()
+        {
+            //构造一个Autofac的Builder容器
+            var builder = new ContainerBuilder();
+            #region 注册程序集
+            //注册程序集，有接口的可以用接口来保存
+            Assembly controllerAss = Assembly.Load("MicroERP.Web");
+            builder.RegisterControllers(controllerAss);
+            builder.RegisterFilterProvider();
+            Assembly dal = Assembly.Load("MicroERP.DAL");
+            Type[] dalTypes = dal.GetTypes();
+            builder.RegisterTypes(dalTypes).AsImplementedInterfaces();
+            Assembly bll = Assembly.Load("MicroERP.BLL");
+            builder.RegisterTypes(bll.GetTypes());
+            #endregion
+            //创造Autofac工作容器的实例
+            var container = builder.Build();
+            //替换MVC默认的控制器工厂
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
--- MicroERP.Web/Global.asax.cs
+++ MicroERP.Web/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using MicroERP.DAL;
+using MicroERP.Web.App_Start;
 
 namespace MicroERP.Web
 {
@@ -14,10 +15,13 @@ namespace MicroERP.Web
 		protected void Application_Start()
 		{
 			AreaRegistration.RegisterAllAreas();
+            AutofacConfig.Register();//AutoFac注入
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
-
+            //清理视图引擎，只保留Razor
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
             ////自动建立数据库
             //using (var Context = new MicroERPContext())
             //{
