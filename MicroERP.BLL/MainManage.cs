f63df6e56b6a313fe6dce36a598a8a9f100b479e
ZoraGK <jyh199759@126.com>
2019-06-02 07:11:22 +0800

    临时提交

--- MicroERP.BLL/MainManage.cs
+++ MicroERP.BLL/MainManage.cs
@@ -18,7 +18,7 @@ namespace MicroERP.BLL
         public MainIndexViewModel GetMainIndex()
         {
             MainIndexViewModel data = new MainIndexViewModel();
-            data.EmployeeCount = userData.GetUserAsEmployee().FindAll(c => c.UserStatus == "在职").Count-1;
+            data.EmployeeCount = userData.GetUserAsEmployee().FindAll(c => c.UserStatus == "在职").Count - 1;
             data.OrderCount = goodsData.GetAllGoodsOrder().Count();
             data.TodoCount = goodsData.GetAllGoodsConfirm().Count();
             data.UnderCheckCount = goodsData.GetAllGoodsRejectedOrder().Count();
@@ -27,31 +27,45 @@ namespace MicroERP.BLL
         public TableViewModel GetMainTable()
         {
             TableViewModel data = new TableViewModel();
-            var table = goodsData.GetAllGoodsOrder();
-            if(table.Count>13)
-            table = table.GetRange(1, 13);
-            table = table.OrderByDescending(c => c.OrderTime).ToList();
-            int i = 0;
-            foreach (var item in table)
+            //var table = goodsData.GetAllGoodsOrder();
+            //if(table.Count>13)
+            //table = table.GetRange(1, 13);
+            //table = table.OrderByDescending(c => c.OrderTime).ToList();
+            //int i = 0;
+            //foreach (var item in table)
+            //{
+            //    if (String.IsNullOrEmpty(data.TDate[i]))
+            //    {
+            //        data.TDate[i] = item.OrderTime.Date.ToString();
+            //        data.TNumbers[i] = 0;
+            //    }
+            //    if (data.TDate[i] == item.OrderTime.Date.ToString())
+            //    {
+            //        data.TNumbers[i] = data.TNumbers[i] + item.GoodsQuantity;
+            //    }
+            //    else
+            //    {
+            //        i += 1;
+            //        data.TDate[i] = item.OrderTime.Date.ToString();
+            //        data.TNumbers[i] = 0;
+            //        data.TNumbers[i] = data.TNumbers[i] + item.GoodsQuantity;
+            //    }
+            //}
+            data.TNumbers = new int[8];
+            data.TDate = new string[8];
+            for(int i=0;i<8;i++)
             {
-                if (String.IsNullOrEmpty(data.TDate[i]))
-                {
-                    data.TDate[i] = item.OrderTime.Date.ToString();
-                    data.TNumbers[i] = 0;
-                }
-                if (data.TDate[i] == item.OrderTime.Date.ToString())
-                {
-                    data.TNumbers[i] = data.TNumbers[i] + item.GoodsQuantity;
-                }
-                else
-                {
-                    i += 1;
-                    data.TDate[i] = item.OrderTime.Date.ToString();
-                    data.TNumbers[i] = 0;
-                    data.TNumbers[i] = data.TNumbers[i] + item.GoodsQuantity;
-                }
+                data.TNumbers[i] = randomNumber(i);
+                int day = i + 15;
+                data.TDate[i] = "5月" + day + "日";
             }
             return data;
+        }
+        int randomNumber(int i)
+        {
+            int ret=0;
+            ret = (int)(Math.Cos(i)-Math.Sin(i)+1.5)*10000;
+            return ret;
         }
     }
 }
